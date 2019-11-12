01113e852044085b1d3fa11313f2c25ce445dfc0
karlssonr <55179395+karlssonr@users.noreply.github.com>
2019-11-12 14:03:21 +0100

    update
    update

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-// 1
+
 
 var bana = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -9,28 +9,63 @@ var bana = [
     [1,2,1,2,1,2,1,2,2,2,2,2,2,2,2,1],
     [1,2,1,2,2,2,2,2,1,1,1,1,1,1,2,1],
     [1,2,1,1,1,1,1,1,1,2,2,2,2,2,2,1],
-    [1,2,2,2,1,2,2,2,2,2,1,1,1,1,1,1],
+    [1,2,2,2,1,2,2,2,2,2,1,2,1,1,1,1],
     [1,1,1,2,1,2,1,1,1,2,1,2,2,1,2,4],
     [1,2,2,2,1,2,1,2,2,2,1,1,2,2,2,1],
     [1,2,1,1,1,2,1,2,1,1,1,1,1,1,2,1],
     [1,2,2,2,2,2,1,2,2,2,2,2,2,2,2,1],
-    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    [1,2,1,2,1,1,1,2,1,1,1,2,1,1,1,1],
+    [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    
 ]
+//positon katt
 var kattx = 1;
 var katty = 1;
 
+//position monster1
 var monsterx = 8;
 var monstery = 3;
-var eastWest = 1;  
-var isPaused = false;
+
+//position monster2
+ var monster2x = 14;
+ var monster2y = 10;
+
+ //position monster3
+ var monster3x = 11;
+ var monster3y = 12;
+
+var eastWest3 = 1;
+var eastWest2 = 1;
+var eastWest  = 1;
+  
+var isPaused  = false;
 
 function update() {
     if (isPaused == false){
-    monsterx = monsterx + eastWest;
+    monsterx  = monsterx  + eastWest;
+    monster2x = monster2x + eastWest2;
+    monster3x = monster3x + eastWest3;
+
    
     if(bana[monstery][monsterx]  === 1 ){
-        eastWest = eastWest * (-1)
-        monsterx = monsterx + 2 * eastWest;
+        eastWest  = eastWest  * (-1)
+        monsterx  = monsterx  + 2 * eastWest;
+
+
+    }
+    if(bana[monster2y][monster2x]  === 1 ){
+       
+        eastWest2 = eastWest2 * (-1)
+        monster2x = monster2x + 2 * eastWest2;
+
+    }
+
+    if(bana[monster3y][monster3x]  === 1 ){
+       
+        eastWest3 = eastWest3 * (-1)
+        monster3x = monster3x + 2 * eastWest3;
+
     }
 
     
@@ -40,8 +75,15 @@ function update() {
 
 function pause(){
 
+    
+
     if (isPaused == true){
         isPaused = false
+        // var pausad = document.createElement("pausad");
+        // pausad.innerHTML = "pausad";
+        // document.body.prepend(pausad);
+        // pausad.classList.add("pausad");
+        // console.log(pausad.innerHTML);
     }
     else{
         isPaused = true
@@ -51,20 +93,33 @@ function pause(){
 
 
 
+
+
     
 
 
 function draWorld() {
-    document.getElementById("spelruta").innerHTML = "";
+    document.getElementById("spelruta_p").innerHTML = "";
     for(var y = 0; y < bana.length; y=y+1){
         for(var x=0; x < bana[y].length; x=x+1){
 
             if (y===monstery&&x===monsterx){
-                document.getElementById("spelruta").innerHTML += "<img src='monster.png' class='monster bilder'>";
+                document.getElementById("spelruta_p").innerHTML += "<img src='monster.png' class='monster bilder'>";
                 continue
             }
+
+            if (y===monster2y&&x===monster2x){
+                document.getElementById("spelruta_p").innerHTML += "<img src='monster.png' class='monster bilder'>";
+                continue
+            }
+
+            if (y===monster3y&&x===monster3x){
+                document.getElementById("spelruta_p").innerHTML += "<img src='monster.png' class='monster bilder'>";
+                continue
+            }
+
             if (y===katty&&x===kattx){
-                document.getElementById("spelruta").innerHTML += "<img src='cat.png' class='cat bilder'>";
+                document.getElementById("spelruta_p").innerHTML += "<img src='cat.png' class='cat bilder'>";
                 continue
             }
 
@@ -72,13 +127,13 @@ function draWorld() {
 
             else{
                 if(bana[y][x] ===1){
-                    document.getElementById("spelruta").innerHTML += "<img src='wall.png' class='wall bilder'>";
+                    document.getElementById("spelruta_p").innerHTML += "<img src='wall.png' class='wall bilder'>";
                 }
                 else  if(bana[y][x] ===2){
-                    document.getElementById("spelruta").innerHTML += "<div <img src='grå.png' class='marken bilder'></div>";
+                    document.getElementById("spelruta_p").innerHTML += "<div <img src='grå.png' class='marken bilder'></div>";
                 }
                 else  if(bana[y][x] ===4){
-                    document.getElementById("spelruta").innerHTML += "<div class='mål bilder'></div>";
+                    document.getElementById("spelruta_p").innerHTML += "<div class='mål bilder'></div>";
                 }
         }//else
     }//xlength
@@ -105,6 +160,22 @@ if (katty===monstery&&kattx===monsterx){
         finish.classList.add("finish");
         console.log(finish.innerHTML);
     }//finish
+
+    
+
+    // if (isPaused = true){
+
+    //     var pausad = document.createElement("pausad");
+        
+    //     document.body.prepend(pausad);
+    //     pausad.classList.add("pausad");
+    //     console.log(pausad.innerHTML);
+        
+    // }//pausad
+
+
+
+
 
 }//drawworld
 
